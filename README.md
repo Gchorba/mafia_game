2074e28d76d7c3078a9e4d9e7842124bf0b79569
Pooja Pasawala <pooja@basicer.com>
2014-02-14 23:59:53 -0500

    Delete unnecessary files and update sql commands

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 mafia_game
 ==========
 
-Mafia/Werewolf Game
+Mafia/Werewolf Style Game. Characters are assigned at random to a list of players. Used twilio to text message players their roles.
+
+
--- schema.sql
+++ schema.sql
@@ -4,7 +4,7 @@ id integer NOT NULL Primary Key AUTO_INCREMENT,
 role TEXT,
 description TEXT,
 sees_mafia BOOLEAN
-)
+);
 
 
 DROP TABLE students;
@@ -14,7 +14,7 @@ name TEXT,
 email TEXT,
 phone TEXT,
 is_playing BOOLEAN
-)
+);
 
 
 INSERT INTO roles (role, description, sees_mafia) VALUES 
@@ -27,42 +27,38 @@ INSERT INTO roles (role, description, sees_mafia) VALUES
 ("Informant", "You know who is in the mafia but the mafia don't know you.", true),
 ("Rambo", "You can throw a grenade that kills a person + the 2 people on either side of them at any one point in the game.", false),
 ("Peeping Tom", "You can open your eyes at any point during the game, but don't get caught or you'll end up dead.", false),
-("Postman", "When you are killed, you can take someone else down with you.", false)
+("Postman", "When you are killed, you can take someone else down with you.", false);
 
 
 INSERT INTO students (name, email, phone, is_playing) VALUES 
 ("Ashley Williams", "ashley@flatironschool.com", "+19084182532", true),
 ("Blake Johnson", "blake@flatironschool.com", "+19176000414", true),
-("Amine Tourki", "atourki@gmail.com", "+12037161196", true),
+("Amine Tourki", "atourki@gmail.com", "+12037161196", false),
 ("Anisha Ramnani", "anisha.ramnani@gmail.com", "+14089609931", true),
-("Ariel Isaacs", "ariel.isaacs@gmail.com", "+14154257945", true),
+("Ariel Isaacs", "ariel.isaacs@gmail.com", "+14154257945", false),
 ("Armando Amador", "Arm.amador@gmail.com", "+19175432702", true),
 ("Chris Haack", "chris.haack@me.com", "+13126360711", true),
 ("David Gabeau", "djg400@nyu.edu", "+12405933853", true),
-("Derek Silverman", "dereksilverman@gmail.com", "+19417371525.", true),
+("Derek Silverman", "dereksilverman@gmail.com", "+19417371525.", false),
 ("Felicia O'Garro", "felicia.ogarro@gmail.com", "+13474500396", true),
 ("Gustavo Guimaraes", "gustavoguimaraes@gmail.com", "+19207502147", true),
 ("Hannah Nordgren", "hannah.nordgren@gmail.com", "+19176370655", true),
 ("Jahmil Eady", "jahmileady@gmail.com", "+19172328481", true),
-("Jeremy Lewis", "Lewis.JP@gmail.com", "+13476020692", true),
+("Jeremy Lewis", "Lewis.JP@gmail.com", "+13476020692", false),
 ("Jonathan Brown", "jonathan.brown.a@gmail.com", "+16467645425", true),
 ("Kate Donahue", "katepdonahue@gmail.com", "+15103167758", true),
 ("Katie Hoffman", "ktahoffman@gmail.com", "+17202327274", true),
 ("Keith Williams Jr.", "keithwilliamsjr@gmail.com", "+12679187836", true),
-("Keturah Walters", "ketwalters@gmail.com", "+19174340078", true),
+("Keturah Walters", "ketwalters@gmail.com", "+19174340078", false),
 ("Nikki Thean", "nikki.thean@gmail.com", "+16097512360", true),
 ("Nisha Batra", "nishabatra27@gmail.com", "+19174990390", true),
 ("Oliver Switzer", "oliverswitzer@gmail.com", "+13475811089", true),
 ("Pooja Pasawala", "pooja@basicer.com", "+19739313192", true),
 ("Sara Gorecki", "sgorecki@gmail.com", "+15165676941", true),
 ("Sarah Ranso", "sranso@gmail.com", "+19196324696", true),
-("Sterling Walker", "breset.walker@flatironschool.com", "+19257083984", false),
+("Sterling Walker", "breset.walker@gmail.com", "+19257083984", true),
 ("Sean Reed", "seanreed1111@gmail.com", "+19174149644", true),
-("Tessa Fallon", "tessa.fallon@gmail.com", "+1", false),
-("Uzo Ukeagu", "msuzoagu.@gmail.com", "+19712226803", true),
-("Vois Cornerstone", "vois.cornerstone@gmail.com", "+1", false)
-
-
-#need phone from tessa and vois
-#need email from sterling
+("Tessa Fallon", "tessa.fallon@gmail.com", "+16467079363", true),
+("Uzo Ukeagu", "msuzoagu.@gmail.com", "+19712226803", false),
+("Vois Cornerstone", "vois.cornerstone@gmail.com", "+19174681347", true);
 
