ada5e505285d371439c0e352f4940b719ce404f6
Pooja Pasawala <pooja@basicer.com>
2014-03-03 17:16:53 -0500

    Add twilio stuffs

--- app.rb
+++ app.rb
@@ -1,21 +1,50 @@
 require 'bundler'
 Bundler.require
 require './lib/game.rb'
+require './lib/player_list.rb'
+require './lib/mafia_list.rb'
+require './lib/character_list.rb'
+require './lib/civilian_list.rb'
 require 'twilio-ruby'
 
 class App < Sinatra::Application
 
-  account_sid = ''
-  auth_token = ''
-  @client = Twilio::REST::Client.new account_sid, auth_token
+  # currently only includes test sid/token/number
+  account_sid = "AC24cbd663ebeb7a86d0219d62206409a8"
+  # ENV['TWILIO_ACCOUNT_SID']
+  auth_token = "e12190dedbe20350bafb19f062edec98"
+  # ENV['TWILIO_AUTH_TOKEN']
+  twilio_number = "+15005550006"
+  # ENV['TWILIO_NUMBER']
+  twilio = Twilio::REST::Client.new account_sid, auth_token
 
-  @short_code = @client.account.sms.short_codes.get("")
-  puts @short_code.short_code
-  
-  @client.account.messages.create(
-    :from => '+',
-    :to => '+',
-    :body => 
-  )
+  from  = twilio_number
+
+  notifications = {
+    "+19739313192" => "body",
+    "+14782274237" => "body2"
+  }
+
+  notifications.each do |key, value|
+    twilio.account.messages.create(
+      :from => from,
+      :to => key,
+      :body => value
+      )
+    puts "Sent message to #{key}"
+  end
+
+  # sms = twilio.account.sms.messages.create(:body => "All in the game, yo",
+  #   :to => "+14108675309",
+  #   :from => "+15005550006")
+  # puts sms.body
+
+  # post "/" do
+  #   twilio.account.messages.create(
+  #     :from => twilio_number,
+  #     :to => '+1',
+  #     :body => 
+  #   )
+  # end
 
 end
--- lib/game.rb
+++ lib/game.rb
@@ -5,31 +5,21 @@ require './civilian_list.rb'
 
 class MafiaGame
 
-  attr_accessor :students, :count, :player_list
+  attr_accessor :students, :count
   ROLES = []
 
   def initialize(array)
     @students = array
     @count = 0
-    @player_list = []
   end
 
   def player_count
-    students.each do |info|
+    @students.each do |info|
       if info[3] == true
         @count += 1 
       end
     end
     @count
-  end
-
-  def students_playing
-    students.each do |info|
-      if info[3] == true
-        player_list << info 
-      end
-    end
-    player_list
   end
 
   def mafia_roles
@@ -46,17 +36,16 @@ class MafiaGame
     end
   end
 
-  def people(chars)
-    civilians = player_count - mafia_roles - civilian_roles
-    chars(0..civilians).each do |role| 
+  def civilians(char_array)
+    total_civilians = player_count - mafia_roles - civilian_roles
+    char_array(0..total_civilians).each do |role| 
       ROLES << role
     end
   end
 
   def assign_roles
-    player_list.shuffle!
-    ROLES.shuffle!
-    puts ROLES.zip(player_list)
+    @students.shuffle!
+    ROLES.pop
   end
 
   def twilio_API_calls
@@ -64,38 +53,6 @@ class MafiaGame
 
 end
 
-player_array = [
-  ["Ashley Williams", "ashley@flatironschool.com", "+19084182532", true],
-  ["Blake Johnson", "blake@flatironschool.com", "+19176000414", true],
-  ["Amine Tourki", "atourki@gmail.com", "+12037161196", false],
-  ["Anisha Ramnani", "anisha.ramnani@gmail.com", "+14089609931", true],
-  ["Ariel Isaacs", "ariel.isaacs@gmail.com", "+14154257945", false],
-  ["Armando Amador", "Arm.amador@gmail.com", "+19175432702", true],
-  ["Chris Haack", "chris.haack@me.com", "+13126360711", true],
-  ["David Gabeau", "djg400@nyu.edu", "+12405933853", true],
-  ["Derek Silverman", "dereksilverman@gmail.com", "+19417371525.", false],
-  ["Felicia O'Garro", "felicia.ogarro@gmail.com", "+13474500396", true],
-  ["Gustavo Guimaraes", "gustavoguimaraes@gmail.com", "+19207502147", true],
-  ["Hannah Nordgren", "hannah.nordgren@gmail.com", "+19176370655", true],
-  ["Jahmil Eady", "jahmileady@gmail.com", "+19172328481", true],
-  ["Jeremy Lewis", "Lewis.JP@gmail.com", "+13476020692", false],
-  ["Jonathan Brown", "jonathan.brown.a@gmail.com", "+16467645425", true],
-  ["Kate Donahue", "katepdonahue@gmail.com", "+15103167758", true],
-  ["Katie Hoffman", "ktahoffman@gmail.com", "+17202327274", true],
-  ["Keith Williams Jr.", "keithwilliamsjr@gmail.com", "+12679187836", true],
-  ["Keturah Walters", "ketwalters@gmail.com", "+19174340078", false],
-  ["Nikki Thean", "nikki.thean@gmail.com", "+16097512360", true],
-  ["Nisha Batra", "nishabatra27@gmail.com", "+19174990390", true],
-  ["Oliver Switzer", "oliverswitzer@gmail.com", "+13475811089", true],
-  ["Pooja Pasawala", "pooja@basicer.com", "+19739313192", true],
-  ["Sara Gorecki", "sgorecki@gmail.com", "+15165676941", true],
-  ["Sarah Ranso", "sranso@gmail.com", "+19196324696", true],
-  ["Sterling Walker", "breset.walker@gmail.com", "+19257083984", true],
-  ["Sean Reed", "seanreed1111@gmail.com", "+19174149644", true],
-  ["Tessa Fallon", "tessa.fallon@gmail.com", "+16467079363", true],
-  ["Uzo Ukeagu", "msuzoagu.@gmail.com", "+19712226803", false],
-  ["Vois Cornerstone", "vois.cornerstone@gmail.com", "+19174681347", true]
-]
 char_array = [
       ["Sheriff", "You're the sheriff. You get to find out about one person each night phase, unless you have been killed.", false],
       ["Doctor", "You're the Doctor. During each night phase you get to choose a player to protect, unless you have been killed.", false],
@@ -106,10 +63,12 @@ char_array = [
       ["Postman", "You are the Postman. When you are killed, you can take someone else down with you.", false]
 ]
 
-my_game = MafiaGame.new(player_array)
-my_game.player_count
-my_game.students_playing
-my_game.mafia_roles
-my_game.civilian_roles
-my_game.people(char_array)
-my_game.assign_roles
+# possible_players = Player.new
+# players = MafiaGame.new(possible_players.playing)
+# my_game.player_count
+# my_game.students_playing
+# my_game.mafia_roles
+# my_game.civilian_roles
+# my_game.people(char_array)
+# my_game.assign_roles
+
